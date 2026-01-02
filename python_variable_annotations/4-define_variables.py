2d0ec19c37d6b56b588c48cd5ac06901d67ac700
Ayse <ayse@example.com>
2026-01-02 19:03:18 +0000

    Add Task 4: Define variables with type annotations

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Define and annotate variables with specified values and types.
+"""
+
+# Variable definitions with type annotations
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
