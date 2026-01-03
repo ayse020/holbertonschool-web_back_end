d10c06df8858516a7b98a5fa75ab6ff17cc2ebf5
Ayse <ayse@example.com>
2026-01-03 20:47:53 +0000

    Task 9: Create groceriesList function that returns a map of groceries

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+// 9-groceries_list.js
+
+/**
+ * Returns a map of groceries with specified items and quantities
+ * @returns {Map} A Map containing grocery items and their quantities
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+}
